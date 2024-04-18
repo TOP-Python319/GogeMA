daf7f6a7236a39c6aee19e07b23a2f80bfe172df
Maria Goge <gogemaria@mail.ru>
2024-04-19 00:36:18 +0800

    test is done

---
+++ 1.py
@@ -0,0 +1,11 @@
+# def strong_password(str):
+#     len(str) >= 8 and 
+
+print('Java')
+print('Ruby')
+print('Scala')
+print('Python', end='+')  # print('C++')
+print('GO')
+print('C#', end='=')  # print('C')
+print('awesome')
+# finish
